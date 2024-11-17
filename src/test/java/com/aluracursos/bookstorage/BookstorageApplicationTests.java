9f48abe225667e22ce1b42b98fe837b2735f63e8
Ricardo Umana <u3dide@gmail.com>
2024-11-17 18:38:36 -0500

    primer commit adicionando archivos

---
+++ src/test/java/com/aluracursos/bookstorage/BookstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.bookstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
